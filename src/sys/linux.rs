431ee2416e57b80de223f6d166d9d53ca46a5ca0
Dilawar Singh <dilawar@subcom.tech>
2024-07-17 01:27:58 +0530

    feat: Add support for nmcli (#2)
    * chore: use nmcli by default
    
    * test: Fixes to tests on Linux
    
    * feat: Add support for nmcli
    
    Version bumped to 0.6.0

--- src/sys/linux.rs
+++ src/sys/linux.rs
@@ -2,8 +2,63 @@ use crate::{Error, Result, Wifi};
 use std::env;
 use std::process::Command;
 
-/// Returns a list of WiFi hotspots in your area - (Linux) uses `iw`
+/// Returns a list of WiFi hotspots in your area - (Linux). uses `nmcli` or `iw`.
 pub(crate) fn scan() -> Result<Vec<Wifi>> {
+    scan_nm().and_then(|_| scan_iw())
+}
+
+/// Returns a list of WiFi hotspots in your area - (Linux) uses `nmcli`
+fn scan_nm() -> Result<Vec<Wifi>> {
+    let output = Command::new("nmcli")
+        .arg("--color")
+        .arg("no")
+        .arg("--terse")
+        .arg("-f")
+        .arg("ssid,chan,signal,security,bssid")
+        .arg("dev")
+        .arg("wifi")
+        .arg("list")
+        .output()
+        .map_err(|_| Error::CommandNotFound)?;
+
+    let data = String::from_utf8_lossy(&output.stdout);
+
+    let mut result = vec![];
+    for line in data.lines() {
+        let mut wifi = Wifi::default();
+        let mut fs = line.splitn(5, ':');
+        if let Some(ssid) = fs.next() {
+            wifi.ssid = ssid.to_string();
+        } else {
+            continue;
+        }
+        if let Some(channel) = fs.next() {
+            wifi.channel = channel.to_string();
+        } else {
+            continue;
+        }
+        if let Some(signal_level) = fs.next() {
+            wifi.signal_level = signal_level.to_string();
+        } else {
+            continue;
+        }
+        if let Some(security) = fs.next() {
+            wifi.security = security.to_string();
+        } else {
+            continue;
+        }
+        if let Some(mac) = fs.next() {
+            wifi.mac = mac.replace(r"\:", ":").to_string();
+        } else {
+            continue;
+        }
+        result.push(wifi);
+    }
+    Ok(result)
+}
+
+/// Returns a list of WiFi hotspots in your area - (Linux) uses `iw`
+fn scan_iw() -> Result<Vec<Wifi>> {
     const PATH_ENV: &'static str = "PATH";
     let path_system = "/usr/sbin:/sbin";
     let path = env::var_os(PATH_ENV).map_or(path_system.to_string(), |v| {
@@ -50,7 +105,7 @@ fn parse_iw_dev(interfaces: &str) -> Result<String> {
 fn parse_iw_dev_scan(network_list: &str) -> Result<Vec<Wifi>> {
     let mut wifis: Vec<Wifi> = Vec::new();
     let mut wifi = Wifi::default();
-    for line in network_list.split("\n") {
+    for line in network_list.lines() {
         if let Ok(mac) = extract_value(line, "BSS ", Some("(")) {
             if !wifi.mac.is_empty()
                 && !wifi.signal_level.is_empty()
@@ -104,19 +159,22 @@ mod tests {
     use std::path::PathBuf;
 
     #[test]
+    fn test_nmcli() {
+        if let Err(e) = Command::new("nmcli").arg("--version").output() {
+            eprintln!("nmlci is not found: {e}");
+            return;
+        }
+        let wifis = scan_nm().expect("failed to scan");
+        println!("Wifis: {wifis:?}");
+    }
+
+    #[test]
     fn should_parse_iw_dev() {
         let expected = "wlp2s0";
 
         // FIXME: should be a better way to create test fixtures
-        let mut path = PathBuf::new();
-        path.push("tests");
-        path.push("fixtures");
-        path.push("iw");
-        path.push("iw_dev_01.txt");
-
-        let file_path = path.as_os_str();
-
-        let mut file = File::open(&file_path).unwrap();
+        let path = PathBuf::from("tests/fixtures/iw/iw_dev_01.txt");
+        let mut file = File::open(&path).unwrap();
 
         let mut filestr = String::new();
         let _ = file.read_to_string(&mut filestr).unwrap();
@@ -133,7 +191,7 @@ mod tests {
             ssid: "hello".to_string(),
             channel: "10".to_string(),
             signal_level: "-67.00".to_string(),
-            security: "".to_string(),
+            security: "PSK".to_string(),
         });
 
         expected.push(Wifi {
@@ -141,25 +199,17 @@ mod tests {
             ssid: "hello-world-foo-bar".to_string(),
             channel: "8".to_string(),
             signal_level: "-89.00".to_string(),
-            security: "".to_string(),
+            security: "PSK".to_string(),
         });
 
         // FIXME: should be a better way to create test fixtures
-        let mut path = PathBuf::new();
-        path.push("tests");
-        path.push("fixtures");
-        path.push("iw");
-        path.push("iw_dev_scan_01.txt");
-
-        let file_path = path.as_os_str();
-
-        let mut file = File::open(&file_path).unwrap();
-
+        let path = PathBuf::from("tests/fixtures/iw/iw_dev_scan_01.txt");
+        let mut file = File::open(&path).unwrap();
         let mut filestr = String::new();
         let _ = file.read_to_string(&mut filestr).unwrap();
 
         let result = parse_iw_dev_scan(&filestr).unwrap();
         assert_eq!(expected[0], result[0]);
-        assert_eq!(expected[1], result[5]);
+        assert_eq!(expected[1], result[4]);
     }
 }
